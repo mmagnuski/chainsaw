12966f151a37a81b01dad76aafb3fe962203bc94
mmagnuski <mmagnuski@swps.edu.pl>
2022-01-05 19:13:45 +0100

    temporary: add colors to stim_utils

--- stim_utils.py
+++ stim_utils.py
@@ -47,3 +47,21 @@ def feedback_circles(window, radius=1.5, units='deg'):
     circ_corr = fix(window, color=tuple(feedback_colors[0, :]), **args)
     circ_incorr = fix(window, color=tuple(feedback_colors[1, :]), **args)
     return circ_corr, circ_incorr
+
+
+# colors
+# ------
+
+# equiluminant colors (for phase info for example)
+r = np.array([0.718, 0.847, 0.527, 0.000, 0.000, 0.316, 0.718])
+g = np.array([0.000, 0.057, 0.527, 0.592, 0.559, 0.316, 0.000])
+b = np.array([0.718, 0.057, 0.000, 0.000, 0.559, 0.991, 0.718])
+rgb = np.stack([r, g, b], axis=1)
+
+# colors - maybe name all equiluminant colors and provide (or use abvailable)
+# color converters (for example 0 - 1 normalized to -1 - 1 psychopy norm)
+# or rgb255 to psychopy -1 - 1 normalized)
+colors_norm = {'pink': [0.718, 0, 0.718], 'blue': [0.316, 0.316, 0.991]}
+colors_255 = {'pink': [183, 0, 183], 'blue': [81, 81, 253]}
+colors_psychopy = {'pink': [0.436, -1., 0.436],
+                   'blue': [-0.368, -0.368, 0.982]}
